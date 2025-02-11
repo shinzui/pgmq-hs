3a044c110735de8ffde90678d82511c6e4e96daa
Nadeem Bitar <nadeem@gmail.com>
2025-02-11 07:28:21 -0800

    Add queue name parser

--- src/Pgmq/Types.hs
+++ src/Pgmq/Types.hs
@@ -1,6 +1,15 @@
-module Pgmq.Types where
+module Pgmq.Types
+  ( MessageBody (..),
+    MessageId (..),
+    Message (..),
+    QueueName,
+    parseQueueName,
+  )
+where
 
 import Data.Aeson (Value)
+import Data.Char (isAlphaNum, isAscii)
+import Data.Text qualified as T
 import Pgmq.Prelude
 
 newtype MessageBody = MessageBody {unMessageBody :: Value}
@@ -19,3 +28,27 @@ data Message = Message
     body :: !MessageBody
   }
   deriving stock (Eq, Generic, Show)
+
+newtype QueueName = QueueName Text
+  deriving newtype (Eq, Ord, FromJSON, ToJSON)
+  deriving stock (Show, Generic)
+
+newtype PgmqError = InvalidQueueName Text
+  deriving stock (Show, Generic)
+
+-- Adopted from https://github.com/tembo-io/pgmq/blob/e4d4b84bf302df77be2d1f877c5cf8ef8861bfc7/pgmq-rs/src/util.rs#L94
+parseQueueName :: Text -> Either PgmqError QueueName
+parseQueueName t
+  | not isShortEnough = Left $ InvalidQueueName "The queue name is too long."
+  | not hasValidCharacters = Left $ InvalidQueueName "The queue name contains invalid characters."
+  | otherwise = Right $ QueueName t
+  where
+    isShortEnough = T.length t <= maxQueueNameLength
+    hasValidCharacters = T.all isValidChar t
+    isValidChar c = (isAscii c && isAlphaNum c) || c == '_'
+
+    -- PostgreSQL identifier length information
+    -- https://www.postgresql.org/docs/current/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS
+    maxIdentifierLength = 63 -- PostgreSQL truncates beyond this length
+    longestPrefix :: Text = "archived_at_idx_"
+    maxQueueNameLength = maxIdentifierLength - T.length longestPrefix
